1e35b8ef3673aafc1be99d0b7cd36ba2c33540de
Samuel Solberg <pauliesays65@gmail.com>
2018-09-11 14:39:11 -0500

    routes added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-
+const snakeRouter = require('./routes/snake.router');
 
 const port = 5000;
 
-app.get('/snakes', (req, res) => {
-    res.send([]);
-})
+app.use('/', snakeRouter);
 
 app.listen(port, () => {
     console.log('listening on port: ', port);
